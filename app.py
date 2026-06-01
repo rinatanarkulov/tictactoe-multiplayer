83dcf586f48e29cb14ea2e47c027017ec256e0b5
Rinat Anarkulov <anarkulovrinat@gmail.com>
2026-06-01 03:27:45 +0000

    Add eventlet monkey patch for Redis

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, Response
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import json
