d51c6073b152fda92c0a1a5d6b60a7acc5f36ac6
Rinat Anarkulov <anarkulovrinat@gmail.com>
2026-07-08 04:16:17 +0000

    Fix move validation, add copy button, winning line, mobile fixes

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def create_game_data():
     game_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
     game = {
         'board': [''] * 9,
-        'players': [],
+        'players': {},          # sid -> symbol ('X' or 'O')
         'current_turn': 'X',
         'winner': None,
         'x_moves': [],
@@ -54,8 +54,8 @@ def check_winner(board):
     ]
     for combo in wins:
         if board[combo[0]] == board[combo[1]] == board[combo[2]] != '':
-            return board[combo[0]]
-    return None
+            return board[combo[0]], combo
+    return None, None
 
 # WebSocket events
 @socketio.on('create_game')
@@ -63,7 +63,7 @@ def on_create_game():
     game_id = create_game_data()
     join_room(game_id)
     game = load_game(game_id)
-    game['players'].append(request.sid)
+    game['players'][request.sid] = 'X'
     save_game(game_id, game)
     games_created.inc()
     emit('game_created', {'game_id': game_id, 'symbol': 'X'})
@@ -72,64 +72,91 @@ def on_create_game():
 def on_join_game(data):
     game_id = data['game_id']
     game = load_game(game_id)
-    if game and len(game['players']) < 2:
-        join_room(game_id)
-        game['players'].append(request.sid)
-        save_game(game_id, game)
-        emit('game_joined', {'symbol': 'O'})
-        emit('start_game', {}, room=game_id, include_self=True)
-    else:
-        emit('error', {'message': 'Game full or not found'})
+
+    if not game:
+        emit('error', {'message': 'Game not found'})
+        return
+
+    # Already in this game? just re-send their symbol, don't add again
+    if request.sid in game['players']:
+        emit('game_joined', {'symbol': game['players'][request.sid]})
+        return
+
+    # Game already has 2 players
+    if len(game['players']) >= 2:
+        emit('error', {'message': 'Game is full'})
+        return
+
+    join_room(game_id)
+    game['players'][request.sid] = 'O'
+    save_game(game_id, game)
+    emit('game_joined', {'symbol': 'O'})
+    emit('start_game', {}, room=game_id, include_self=True)
 
 @socketio.on('make_move')
 def on_make_move(data):
     game_id = data['game_id']
     position = data['position']
     game = load_game(game_id)
-    
+
     if not game:
         return
-    
-    if game['board'][position] == '' and not game['winner']:
-        symbol = game['current_turn']
-        moves_key = 'x_moves' if symbol == 'X' else 'o_moves'
-        
-        oldest_position = None
-        if len(game[moves_key]) >= 3:
-            oldest_position = game[moves_key].pop(0)
-            game['board'][oldest_position] = ''
-        
-        game['board'][position] = symbol
-        game[moves_key].append(position)
-        moves_made.inc()
-        
-        opponent_key = 'o_moves' if symbol == 'X' else 'x_moves'
-        next_to_disappear = None
-        if len(game[opponent_key]) >= 3:
-            next_to_disappear = game[opponent_key][0]
-        
-        winner = check_winner(game['board'])
-        
-        if winner:
-            game['winner'] = winner
-            save_game(game_id, game)
-            emit('game_over', {'winner': winner, 'board': game['board']}, room=game_id)
-        else:
-            game['current_turn'] = 'O' if symbol == 'X' else 'X'
-            save_game(game_id, game)
-            emit('move_made', {
-                'board': game['board'],
-                'turn': game['current_turn'],
-                'removed': oldest_position,
-                'fading': next_to_disappear
-            }, room=game_id, include_self=True)
+
+    # Who is this player?
+    symbol = game['players'].get(request.sid)
+    if symbol is None:
+        return  # not a player in this game
+
+    # Not this player's turn -> ignore
+    if symbol != game['current_turn']:
+        return
+
+    # Invalid position or occupied or game over -> ignore
+    if position < 0 or position > 8:
+        return
+    if game['board'][position] != '' or game['winner']:
+        return
+
+    moves_key = 'x_moves' if symbol == 'X' else 'o_moves'
+
+    oldest_position = None
+    if len(game[moves_key]) >= 3:
+        oldest_position = game[moves_key].pop(0)
+        game['board'][oldest_position] = ''
+
+    game['board'][position] = symbol
+    game[moves_key].append(position)
+    moves_made.inc()
+
+    opponent_key = 'o_moves' if symbol == 'X' else 'x_moves'
+    next_to_disappear = None
+    if len(game[opponent_key]) >= 3:
+        next_to_disappear = game[opponent_key][0]
+
+    winner, win_cells = check_winner(game['board'])
+
+    if winner:
+        game['winner'] = winner
+        save_game(game_id, game)
+        emit('game_over', {'winner': winner, 'board': game['board'], 'win_cells': win_cells}, room=game_id)
+    else:
+        game['current_turn'] = 'O' if symbol == 'X' else 'X'
+        save_game(game_id, game)
+        emit('move_made', {
+            'board': game['board'],
+            'turn': game['current_turn'],
+            'removed': oldest_position,
+            'fading': next_to_disappear
+        }, room=game_id, include_self=True)
 
 @socketio.on('reset_game')
 def on_reset_game(data):
     game_id = data['game_id']
     game = load_game(game_id)
     if game:
-        game['players'].reverse()
+        # swap symbols between the two players
+        for sid in game['players']:
+            game['players'][sid] = 'O' if game['players'][sid] == 'X' else 'X'
         game['board'] = [''] * 9
         game['current_turn'] = 'X'
         game['winner'] = None
